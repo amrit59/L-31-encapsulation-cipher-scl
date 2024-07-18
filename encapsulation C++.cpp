be140518519c769e4a7e53df38697f77db567583
Amrit Raj <121791502+amrit59@users.noreply.github.com>
2024-07-18 19:55:33 +0530

    Add files via upload

---
+++ encapsulation C++.cpp
@@ -0,0 +1,24 @@
+
+#include<iostream>
+using namespace std;
+class Student{
+private:
+int marks;
+int attendence;
+string name;
+
+public:
+void displaymarks()
+{
+    cout<<marks;
+}
+};
+
+int main()
+{
+int marks;
+int attendence;
+string name;
+
+}
+
